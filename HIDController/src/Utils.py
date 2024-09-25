ed50276820fea49c936e78d4d09514832748687d
Przemo <przemo06133@gmail.com>
2024-09-25 22:40:45 +0200

    Fix typo, async, loggining

--- HIDController/src/Utils.py
+++ HIDController/src/Utils.py
@@ -1,25 +1,30 @@
-import time
 import random
-from src.HIDKeyMap import HID_KEY_MAP
-from src.consts import SERIAL_PORT, SLEEP_TIME_RANGE
+import asyncio
+import logging
+from HIDKeyMap import HID_KEY_MAP
+from consts import SERIAL_PORT, SLEEP_TIME_RANGE, LOGGER_NAME
+
+logger = logging.getLogger(LOGGER_NAME)
 
 
 class Utils:
     @staticmethod
-    def handle_write_report(instructions: [str]) -> None:
+    async def handle_write_report(instructions: [str]) -> None:
         """Handle all provided instructions to execute"""
         for char in instructions:
+            logger.debug(f"Iter {char}")
             encoded_char = Utils.get_report_value(char)
+            logger.debug("Found encoded_char")
             Utils.write_report(encoded_char)
-            Utils.random_sleep()
+            await Utils.random_sleep()
 
         Utils.write_report("Release")
 
     @staticmethod
     def write_report(report: str) -> None:
         """Write give value into serial port"""
-        with open(SERIAL_PORT, "rb+") as fd:
-            fd.write(report.encode())
+        # with open(SERIAL_PORT, "rb+") as fd:
+        #     fd.write(report.encode())
 
     @staticmethod
     def get_report_value(char: str):
@@ -27,7 +32,9 @@ class Utils:
         return HID_KEY_MAP.get(char)
 
     @staticmethod
-    def random_sleep():
+    async def random_sleep():
         """Sleep for a random duration between 20 and 50 milliseconds"""
         sleep_time = random.uniform(*SLEEP_TIME_RANGE)
-        time.sleep(sleep_time)
+        logger.debug(f"sleep_time: {sleep_time}")
+        await asyncio.sleep(sleep_time)
+
